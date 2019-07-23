69a32789f6bd2bda843f88bcd5b094a248583e06
JaewoongHeo <koolerjaebee@gmail.com>
2019-07-23 16:51:33 +0900

    01python | learn module & error | 190723

---
+++ 01_python/day6.md
@@ -0,0 +1,3 @@
+# 190723 python -day6
+
+`lambda a, b : a+b` : 람다 함수 생성
---
+++ 01_python/test.py
@@ -0,0 +1,11 @@
+import calc
+
+print(calc.add(12, 3))
+
+print(calc.sub(12, 3))
+
+print(calc.multi(12, 3))
+
+print(calc.div(12, 3))
+
+print(calc.div(12, 0))
