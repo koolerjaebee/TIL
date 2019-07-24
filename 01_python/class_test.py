0916b5dba9d7aba4cf51ca793a33425b0af9ccde
JaewoongHeo <koolerjaebee@gmail.com>
2019-07-24 18:20:56 +0900

    01python | learn class | 190724

---
+++ 01_python/class_test.py
@@ -0,0 +1,11 @@
+# 생성자 역시 메서드(함수)기 때문에 추가인자를 받을 수 있습니다.
+class Person:
+    def __init__(self, name):
+        self.name = name
+        print(f'아 응애에요 by {self.name}')
+    
+    def __del__(self):
+        print(f'Rust in pieces - {self.name}')
+        
+
+p = Person('ssafy')
---
+++ 01_python/day7.md
@@ -0,0 +1,16 @@
+# 190724 python -day7
+
+## python naming convention
+
+변수 : 올 소문자 & 띄어쓰기 _
+
+클래스 : 단어 첫 글자 대문자 ex) MyFirstName
+
+
+
+- my_first_name : snake_case
+- MyFirstName : UpperCamelCase
+- myFIrstName : lowerCamelCase
+
+- MYFIRSTNAME : ALLCAPITAL
+
