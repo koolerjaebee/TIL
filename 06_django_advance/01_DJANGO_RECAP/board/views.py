8898e8cf7178b2d5f7ab5969671f12733e6f4fd5
Jaewoong Heo <koolerjaebee@gmail.com>
2019-10-07 15:04:25 +0900

    Implement Create with MTV

--- 06_django_advance/01_DJANGO_RECAP/board/views.py
+++ 06_django_advance/01_DJANGO_RECAP/board/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 
 def index(request):
     return render(request, 'board/index.html')
+
+
+def list(request):
+    articles = Article.objects.all()  # [<A1>, <A2>, <A3>, ...]
+    context = {'articles': articles}
+    return render(request, 'board/list.html', context=context)
+
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+    context = {'article': article}
+    return render(request, 'board/detail.html', context=context)
+
+
+def new(request):
+    return render(request, 'board/new.html')
+
+
+def create(request):
+    article = Article()
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    context = {'article': article}
+    return redirect(f'/board/articles/{article.id}')
