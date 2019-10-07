13faa45d05b139a6ca2c8121c24577b74a2e956c
Jaewoong Heo <koolerjaebee@gmail.com>
2019-10-07 17:49:43 +0900

    06django_advance | make rough site board using sqlite } 191007

--- 06_django_advance/01_DJANGO_RECAP/board/views.py
+++ 06_django_advance/01_DJANGO_RECAP/board/views.py
@@ -1,31 +1,59 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_GET, require_POST
 from .models import Article
 
 
+@require_GET
 def index(request):
     return render(request, 'board/index.html')
 
 
+@require_GET
 def list(request):
     articles = Article.objects.all()  # [<A1>, <A2>, <A3>, ...]
     context = {'articles': articles}
     return render(request, 'board/list.html', context=context)
 
 
+@require_GET
 def detail(request, id):
-    article = Article.objects.get(id=id)
+    article = get_object_or_404(Article, id=id)
     context = {'article': article}
     return render(request, 'board/detail.html', context=context)
 
 
+@require_GET
 def new(request):
     return render(request, 'board/new.html')
 
 
+@require_POST
 def create(request):
     article = Article()
     article.title = request.POST.get('title')
     article.content = request.POST.get('content')
     article.save()
+    return redirect(f'/board/articles/{article.id}/')
+
+
+@require_GET
+def edit(request, id):
+    article = Article.objects.get(id=id)
     context = {'article': article}
-    return redirect(f'/board/articles/{article.id}')
+    return render(request, 'board/edit.html', context=context)
+
+
+@require_GET
+def update(request, id):
+    article = Article.objects.get(id=id)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect(f'/board/articles/{article.id}/')
+
+
+@require_POST
+def delete(request, id):
+    article = Article.objects.get(id=id)
+    article.delete()
+    return redirect('/board/articles/')
---
+++ 06_django_advance/day2.md
@@ -0,0 +1,24 @@
+# 191007 django_advance -day2
+
+## django
+
+### db migrate
+
+1. (대충 Table 만들겠다는 내용)
+
+2. ORM님 검수
+
+3. ORM : 이대로 갑시다. & migrate ex)sqlmigrate
+
+   ```bash
+   $ python manage.py sqlmigrate <appname> <migrationname>
+   ```
+
+4. PROFIT!!
+
+
+
+### extensions
+
+- django_extensions : shell_plus
+- ipython : shell design improvement
