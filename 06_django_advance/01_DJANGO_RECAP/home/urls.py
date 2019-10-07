6ca4271a203ce98c13277e8fe7f3a61120acf5bf
Jaewoong Heo <koolerjaebee@gmail.com>
2019-10-07 10:07:26 +0900

    finish home app & init board app

--- 06_django_advance/01_DJANGO_RECAP/home/urls.py
+++ 06_django_advance/01_DJANGO_RECAP/home/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # /home/
-    path('guess/', views.guess, name='guess'),  # /home/guess/
+    path('', views.index, name='index'),  # home/
+    path('guess/', views.guess, name='guess'),  # home/guess/
     path('answer/', views.answer, name='answer'),  # home/answer/
 ]
--- 06_django_advance/01_DJANGO_RECAP/home/views.py
+++ 06_django_advance/01_DJANGO_RECAP/home/views.py
@@ -11,11 +11,11 @@ def guess(request):
 
 def answer(request):  # 채점 구간
     count = 0
-    if request.GET.get('q1') == '93.4.13':
+    if request.POST.get('q1') == '93.4.13':
         count += 1
-    if request.GET.get('q2') == '250':
+    if request.POST.get('q2') == '250':
         count += 1
-    if request.GET.get('q3') == 'Galaxy S10+':
+    if request.POST.get('q3') == 'Galaxy S10+':
         count += 1
     context = {'count': count}
     return render(request, 'home/answer.html', context=context)
